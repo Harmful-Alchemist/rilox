d43108736e71f5d2a874e9c9b4be80734d134f4d
Harm <el.harmo@gmail.com>
2021-08-26 09:43:24 +0200

    Start environment

---
+++ src/environment.rs
@@ -0,0 +1,26 @@
+use crate::loxvalue::LoxValue;
+use std::collections::HashMap;
+use crate::token::Token;
+
+pub struct Environment {
+    values: HashMap<String, LoxValue>,
+}
+
+impl Environment {
+    pub fn new() -> Self {
+        Environment {
+            values: HashMap::new(),
+        }
+    }
+
+    pub(crate) fn define(&mut self, key: String, value: LoxValue) {
+        self.values.insert(key, value);
+    }
+
+    pub(crate)  fn get(&mut self, name: &Token) -> Result<&LoxValue, String> {
+       match  self.values.get(&*name.lexeme) {
+           None => Err(format!("Undefined variable '{}'.", name.lexeme)),
+           Some(a) => Ok(a)
+       }
+    }
+}
